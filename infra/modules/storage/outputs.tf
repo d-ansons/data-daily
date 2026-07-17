db2dca2ca433f82526db634fc3fe366b476fc90e
Davis <davis.ansons@outlook.com>
2026-07-17 22:33:03 +0100

    devbox updates

--- infra/modules/storage/outputs.tf
+++ infra/modules/storage/outputs.tf
@@ -1,14 +1,14 @@
 output "storage_account_name" {
   description = "Name of the Azure storage account"
-  value = azurerm_storage_account.this.name
+  value       = azurerm_storage_account.this.name
 }
 
 output "storage_account_id" {
   description = "Identifier for the Azure storage account"
-  value = azurerm_storage_account.this.id
+  value       = azurerm_storage_account.this.id
 }
 
 output "primary_blob_endpoint" {
   description = "Base URL of the storage account"
-  value = azurerm_storage_account.this.primary_blob_endpoint
+  value       = azurerm_storage_account.this.primary_blob_endpoint
 }
